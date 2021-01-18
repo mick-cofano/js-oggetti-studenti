db8b0ed6030c62ffffc1e15c5cc3cf0d0eda62f9
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-18 16:08:02 +0100

    Creazione oggetto per descrizione studente

---
+++ script.js
@@ -0,0 +1,14 @@
+// Creare un oggetto che descriva uno studente con le seguenti proprietà:
+// nome, cognome e età. Stampare a schermo attraverso il for tutte le proprietà.
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere
+// un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+
+// creazione dell'oggetto per la descrizione dello studente
+var descrizione = {
+  'nome': 'michele',
+  'cognome': 'cofano',
+  'età': '27'
+}
